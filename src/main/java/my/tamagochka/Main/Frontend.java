56f86e961e0b3c4c2664143b9f4f7732ddda9b8f
tamagochka <tamagochka@gmail.com>
2018-04-15 17:57:47 +0300

    Added class Frontend() and override methods doGet() and doPost().

---
+++ src/main/java/my/tamagochka/Main/Frontend.java
@@ -0,0 +1,27 @@
+package my.tamagochka.Main;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class Frontend extends HttpServlet {
+
+    private String login = "";
+
+    @Override
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse responce)
+            throws ServletException, IOException {
+
+    }
+
+    @Override
+    public void doPost(HttpServletRequest request,
+                       HttpServletResponse responce)
+            throws ServletException, IOException {
+
+    }
+
+}
--- src/main/java/my/tamagochka/Main/Main.java
+++ src/main/java/my/tamagochka/Main/Main.java
@@ -4,11 +4,10 @@ import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 
-import javax.servlet.ServletContainerInitializer;
-
 public class Main {
 
     public static void main(String[] args) throws Exception{
+        Frontend frontend = new Frontend();
         Server server = new Server(8080);
         ServletContextHandler context = new ServletContextHandler();
         server.setHandler(context);
