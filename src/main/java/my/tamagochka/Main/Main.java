ae7b667ab2c6d3b77dad4bfdf55fd1543fa71de7
tamagochka <tamagochka@gmail.com>
2018-04-15 17:31:13 +0300

    Added dependencies to pom.xml jetty-server, jetty-webapp. Added code to main() method.

--- src/main/java/my/tamagochka/Main/Main.java
+++ src/main/java/my/tamagochka/Main/Main.java
@@ -1,8 +1,20 @@
 package my.tamagochka.Main;
 
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+import javax.servlet.ServletContainerInitializer;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws Exception{
+        Server server = new Server(8080);
+        ServletContextHandler context = new ServletContextHandler();
+        server.setHandler(context);
+        context.addServlet(new ServletHolder(frontend), "/authform");
+
+        server.start();
+        server.join();
     }
 }
