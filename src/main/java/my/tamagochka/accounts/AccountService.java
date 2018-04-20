eaa78888203ef5749c08b5b2de6b7937b2fd988c
tamagochka <tamagochka@gmail.com>
2018-04-20 16:32:09 +0300

    Added AccountService class.

---
+++ src/main/java/my/tamagochka/accounts/AccountService.java
@@ -0,0 +1,24 @@
+package my.tamagochka.accounts;
+
+import java.util.HashMap;
+
+public class AccountService {
+
+    private final Map<String, UserProfile> loginToProfile; // bind user login to user profile
+    private final Map<String, UserProfile> sessionToProfile; // bind session id to user profile
+
+    public AccountService() {
+        loginToProfile = new HashMap<>();
+        sessionToProfile = new HashMap<>();
+    }
+
+    public void addNewUser(UserProfile userProfile) { loginToProfile.put(userProfile.getLogin(), userProfile); }
+    public UserProfile getUserByLogin(String login) { return loginToProfile.get(login); }
+    public UserProfile getUserBySession(String session) { return sessionToProfile.get(session); }
+
+    public void addSession(String session, UserProfile userProfile) {
+        sessionToProfile.put(session, userProfile);
+    }
+
+    public void deleteSession(String session) { sessionToProfile.remove(session); }
+}
