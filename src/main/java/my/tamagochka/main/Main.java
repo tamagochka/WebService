bea225842c8150323965ea83509aff51510b8d88
tamagochka <tamagochka@gmail.com>
2018-04-21 14:42:16 +0300

    Added log message "Server started".

--- src/main/java/my/tamagochka/main/Main.java
+++ src/main/java/my/tamagochka/main/Main.java
@@ -26,6 +26,7 @@ public class Main {
         server.setHandler(context);
 
         server.start();
+        java.util.logging.Logger.getGlobal().info("Server started");
         server.join();
     }
 }
