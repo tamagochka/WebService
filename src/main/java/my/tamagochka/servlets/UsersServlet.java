53761dfc5f160cb152d470b4d80320433561963b
tamagochka <tamagochka@gmail.com>
2018-04-21 13:29:23 +0300

    Added doGet, doPost, doPut, doDelete methods to UsersServlet class.

--- src/main/java/my/tamagochka/servlets/UsersServlet.java
+++ src/main/java/my/tamagochka/servlets/UsersServlet.java
@@ -1,4 +1,33 @@
 package my.tamagochka.servlets;
 
-public class UsersServlet {
+import my.tamagochka.accounts.AccountService;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class UsersServlet extends HttpServlet {
+
+    private final AccountService accountService;
+
+    public UsersServlet(AccountService accountService) { this.accountService = accountService; }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // todo: home work
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // todo: home work
+    }
+
+    public void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // todo: home work
+    }
+
+    public void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // todo: home work
+    }
+
 }
