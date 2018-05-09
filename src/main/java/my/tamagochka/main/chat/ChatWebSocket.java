4686fb68761314488dd7f7427361e6308e886f14
tamagochka <tamagochka@gmail.com>
2018-05-09 19:56:29 +0300

    Added ChatWebSocket class and changed index.html.

---
+++ src/main/java/my/tamagochka/main/chat/ChatWebSocket.java
@@ -0,0 +1,43 @@
+package my.tamagochka.main.chat;
+
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketClose;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketConnect;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
+import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+
+@WebSocket
+public class ChatWebSocket {
+
+    private ChatService chatService;
+    private Session session;
+
+    public ChatWebSocket(ChatService chatService) {
+        this.chatService = chatService;
+    }
+
+    @OnWebSocketConnect
+    public void onOpen(Session session) {
+        chatService.add(this);
+        this.session = session;
+    }
+
+    @OnWebSocketClose
+    public void onClose(int statusCode, String reason) {
+        chatService.add(this);
+    }
+
+    @OnWebSocketMessage
+    public void onMessage(String data) {
+        chatService.sendMessage(data);
+    }
+
+    public void sendString(String data) {
+        try {
+            session.getRemote().sendString(data);
+        } catch(Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+}
