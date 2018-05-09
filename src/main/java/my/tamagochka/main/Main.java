24faaa89cd4470a31304ce767e7161fbe22dfb8d
tamagochka <tamagochka@gmail.com>
2018-05-09 19:23:42 +0300

    Changed Main class, added WebServerChatServlet class, and added dependencies.

--- src/main/java/my/tamagochka/main/Main.java
+++ src/main/java/my/tamagochka/main/Main.java
@@ -1,26 +1,29 @@
 package my.tamagochka.main;
 
-import my.tamagochka.accounts.AccountService;
-import my.tamagochka.servlets.SignInServlet;
-import my.tamagochka.servlets.SignUpServlet;
+import my.tamagochka.main.chat.WebSocketChatServlet;
+import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        AccountService accountService = new AccountService();
-
-        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        context.addServlet(new ServletHolder(new SignUpServlet(accountService)), "/signup");
-        context.addServlet(new ServletHolder(new SignInServlet(accountService)), "/signin");
-
         Server server = new Server(8080);
-        server.setHandler(context);
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.addServlet(new ServletHolder(new WebSocketChatServlet()), "/chat");
+
+        ResourceHandler resourceHandler = new ResourceHandler();
+        resourceHandler.setDirectoriesListed(true);
+        resourceHandler.setResourceBase("public_html");
+
+        HandlerList handlers = new HandlerList();
+        handlers.setHandlers(new Handler[] {resourceHandler, context});
+        server.setHandler(handlers);
 
         server.start();
-        java.util.logging.Logger.getGlobal().info("Server started");
         server.join();
     }
 }
