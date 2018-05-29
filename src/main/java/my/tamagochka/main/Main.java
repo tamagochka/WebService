4b4541abbd4d76647aaaccaa25a057ea9f81a631
tamagochka <tamagochka@gmail.com>
2018-05-29 11:35:12 +0300

    Added dependency for log4j. Added logger to Main class, and many any changes. Refactored static page index.html. Removed UsersServlet class.

--- src/main/java/my/tamagochka/main/Main.java
+++ src/main/java/my/tamagochka/main/Main.java
@@ -4,6 +4,8 @@ import my.tamagochka.accounts.AccountService;
 import my.tamagochka.accounts.UserProfile;
 import my.tamagochka.servlets.SessionsServlet;
 import my.tamagochka.servlets.UsersServlet;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.HandlerList;
@@ -11,26 +13,47 @@ import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 
+import javax.management.MBeanServer;
+import javax.management.ObjectName;
+import java.lang.management.ManagementFactory;
+
 public class Main {
 
-    public static void main(String[] args) throws Exception {
-        AccountService accountService = new AccountService();
-        accountService.addNewUser(new UserProfile("admin"));
-        accountService.addNewUser(new UserProfile("test"));
+    static final Logger logger = LogManager.getLogger(Main.class.getName());
 
+    public static void main(String[] args) throws Exception {
+
+        if(args.length != 1) {
+            logger.error("Use port as the first argument");
+            System.exit(1);
+        }
+
+        String portString = args[0];
+        int port = Integer.valueOf(portString);
+
+        logger.info("Starting at http://127.0.0.1:" + portString);
+
+        AccountServerI accountServer = new AccountServer(1);
+
+        AccountServerControllerMBean serverStatistics = new AccountServerController(accountServer);
+        MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
+        ObjectName name = new ObjectName("ServerManager:type=AccountServerController");
+        mbs.registerMBean(serverStatistics, name);
+
+        Server server = new Server(port);
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        context.addServlet(new ServletHolder(new UsersServlet(accountService)), "/api/v1/users");
-        context.addServlet(new ServletHolder(new SessionsServlet(accountService)), "/api/v1/sessions");
+        context.addServlet(new ServletHolder(new HomePageServlet(accountServer)), HomePageServlet.PAGE_URL);
 
         ResourceHandler resourceHandler = new ResourceHandler();
-        resourceHandler.setResourceBase("public_html");
+        resourceHandler.setDirectoriesListed(true);
+        resourceHandler.setResourceBase("static");
 
         HandlerList handlerList = new HandlerList();
         handlerList.setHandlers(new Handler[] {resourceHandler, context});
-        Server server = new Server(8080);
         server.setHandler(handlerList);
 
         server.start();
+        logger.info("Server started");
         server.join();
     }
 }
